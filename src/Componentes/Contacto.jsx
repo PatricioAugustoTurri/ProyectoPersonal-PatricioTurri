35ba8ec0ec9cd6620f435944cb419002b94f7819
PatricioAugustoTurri <patoturri2391@gmail.com>
2024-08-05 07:40:11 -0300

    Update Contacto.jsx

--- src/Componentes/Contacto.jsx
+++ src/Componentes/Contacto.jsx
@@ -1,14 +1,28 @@
 import {Input , Button} from "@nextui-org/react";
 import { Mail } from "lucide-react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Contacto (){
+    const [nombre , setNombre] = useState("");
+    const [apellido , setApellido] = useState("");
+    const [telefono , setTelefono] = useState("");
+    const [email , setEmail] = useState("");
+    const [mensaje , setMensaje] = useState(false);
+
     const enviar = () => {
-        console.log("Enviado")
-        return (
-            <h1>Gracias por contactarnos</h1>
-        )
+        if(nombre === "" || apellido === "" || telefono === "" || email === ""){
+            alert("Todos los campos son requeridos")
+        } else {
+            setMensaje(!mensaje)
+            console.log(mensaje)
+            if(mensaje == true){
+                return (
+                    <h1>Gracias por contactarnos</h1>
+                )
+            }
+        }
     }
+    
     return ( 
         <div>       
             <div className="flex flex-col gap-2 px-32">
@@ -16,11 +30,11 @@ function Contacto (){
                 <p className="text-base">Comunicate conmigo de lo que necesites.</p>
             </div>
             <div className="flex flex-col w-full gap-4 px-40 py-12 md:flex-nowrap">
-                <Input type="text" label="Nombre"/>
-                <Input type="text" label="Apellido"/>
-                <Input type="text" label="Telefono"/>
-                <Input type="email" label="Email"/>
-                <Link to="/contacto/gracias"><Button>Enviar</Button></Link>
+                <Input type="text" label="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
+                <Input type="text" label="Apellido" value={apellido} onChange={(e) => setApellido(e.target.value)}/>
+                <Input type="text" label="Telefono" value={telefono} onChange={(e) => setTelefono(e.target.value)}/>
+                <Input type="email" label="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <Button onClick={enviar}>Enviar</Button>
             </div>
         </div>
     )
