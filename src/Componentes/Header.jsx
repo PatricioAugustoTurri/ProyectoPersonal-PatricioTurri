a9ddb22ecdc639ac63cacdcfc078f73cf9cfac2e
PatricioAugustoTurri <patoturri2391@gmail.com>
2024-08-07 13:22:58 -0300

    -

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,13 +1,24 @@
 import Navegador from "./Navegador";
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "lucide-react";
+import { useContext } from "react";
+import { miContexto } from "../Provider/CarroProvider";
+
 
 function Header (){
+    const cantCarro = useContext(miContexto);
+    
     return (
         <header className="flex flex-col justify-start gap-2 px-5 py-5 text-black sm:items-center sm:justify-between lg:px-48 lg:py-10 sm:flex-row">
-            <Link to="/" className="text-xl font-bold md:text-4xl sm:text-2xl">Pato Turri</Link>
+            <div className="flex items-center justify-between">
+                <Link to="/" className="text-xl font-bold md:text-4xl sm:text-2xl">Pato Turri</Link>
+                <Link className="flex items-center gap-1 text-xl font-bold sm:hidden" to="/carro">{cantCarro.cant}{<ShoppingCart size={22}/>}</Link>
+            </div>
             <Navegador/>
         </header>
     )
 }
 
 export default Header ;
+
+
--- src/Componentes/Navegador.jsx
+++ src/Componentes/Navegador.jsx
@@ -7,6 +7,7 @@ function Navegador (){
     const cantCarro = useContext(miContexto);
     const [isOpen, setIsOpen] = useState(false);
     const [isOpen2, setIsOpen2] = useState(false);
+    const [isOpen3, setIsOpen3] = useState(false);
 
     return (
         <div>
@@ -58,24 +59,24 @@ function Navegador (){
                             <Link to="/" onClick={() => setIsOpen(!isOpen)}>INICIO</Link>
                             <button className="flex justify-between"onClick={() => setIsOpen2(!isOpen2)}>MIS VIAJES<ChevronRight size={20}/></button>
                             <div className={`${isOpen2 ? "w-80" : "w-0"} fixed top-0 left-0 flex flex-col min-h-screen text-sm text-black bg-white transition-all duration-300`}>
-                                <div className={`${!isOpen2 && "hidden"} flex flex-col justify-end gap-4 px-5 py-16`}>
+                                <div className={`${!isOpen2 && "hidden text-transparent"} flex flex-col justify-end gap-4 px-5 py-16 transition-all duration-300 text-black`}>
                                     <button onClick={()=>setIsOpen2(!isOpen2)} className="flex justify-end"><ChevronLeft size={20}/></button>
                                     <Link to="/misviajes/América" onClick={() => {setIsOpen2(!isOpen2) ; setIsOpen(!isOpen)}}>AMERICA</Link>
                                     <Link to="/misviajes/Asia" onClick={() =>{setIsOpen2(!isOpen2) ; setIsOpen(!isOpen)}}>ASIA</Link>
                                     <Link to="/misviajes/Asia" onClick={() => {setIsOpen2(!isOpen2) ; setIsOpen(!isOpen)}}>EUROPA</Link>
                                 </div>
                             </div>
-                            <button className="flex justify-between" onClick={()=>setIsOpen2(!isOpen2)}>MIS FOTOS<ChevronRight size={20}/></button>
-                            <div className={`${isOpen2 ? "w-80" : "w-0"} fixed top-0 left-0 flex flex-col min-h-screen text-sm text-black bg-white transition-all duration-300`} >
-                                <div className={`${!isOpen2 && "hidden"} flex flex-col justify-end gap-4 px-5 py-16`}>
-                                    <button className="flex justify-end" onClick={()=>setIsOpen2(!isOpen2)}><ChevronLeft size={20}/></button>
-                                    <Link to="/misfotos/retratos" className="" onClick={() => {setIsOpen2(!isOpen2) ; setIsOpen(!isOpen)}}>RETRATOS</Link>
-                                    <Link to="/misfotos/paisajes" className="" onClick={() => {setIsOpen2(!isOpen2) ; setIsOpen(!isOpen)}}>PAISAJES</Link>
-                                    <Link to="/misfotos/lh" className="" onClick={() => {setIsOpen2(!isOpen2) ; setIsOpen(!isOpen)}}>LUGARES HISTORICOS</Link>
-                                    <Link to="/misfotos/ciudades" className="" onClick={() => {setIsOpen2(!isOpen2) ; setIsOpen(!isOpen)}}>CIUDADES</Link>
+                            <button className="flex justify-between" onClick={()=>setIsOpen3(!isOpen3)}>MIS FOTOS<ChevronRight size={20}/></button>
+                            <div className={`${isOpen3 ? "w-80" : "w-0"} fixed top-0 left-0 flex flex-col min-h-screen text-sm text-black bg-white transition-all duration-300`} >
+                                <div className={`${!isOpen3 && "hidden text-transparent"} flex flex-col justify-end gap-4 px-5 py-16 transition-all duration-300 text-black`}>
+                                    <button className="flex justify-end" onClick={()=>setIsOpen3(!isOpen3)}><ChevronLeft size={20}/></button>
+                                    <Link to="/misfotos/retratos"  onClick={() => {setIsOpen3(!isOpen3) ; setIsOpen(!isOpen)}}>RETRATOS</Link>
+                                    <Link to="/misfotos/paisajes"  onClick={() => {setIsOpen3(!isOpen3) ; setIsOpen(!isOpen)}}>PAISAJES</Link>
+                                    <Link to="/misfotos/lh" onClick={() => {setIsOpen3(!isOpen3) ; setIsOpen(!isOpen)}}>LUGARES HISTORICOS</Link>
+                                    <Link to="/misfotos/ciudades" onClick={() => {setIsOpen3(!isOpen3) ; setIsOpen(!isOpen)}}>CIUDADES</Link>
                                 </div>
                             </div>
-                            <Link className="flex items-center gap-1" to="/carro" onClick={() => setIsOpen(!isOpen)}>{cantCarro.cant}{<ShoppingCart size={15}/>}</Link>
+                            <Link className="items-center hidden gap-1 sm:block" to="/carro" onClick={() => setIsOpen(!isOpen)}>{cantCarro.cant}{<ShoppingCart size={15}/>}</Link>
                             <Link to="/contacto" onClick={() => setIsOpen(!isOpen)}>CONTACTO</Link>
                         </div>
                     </div>
